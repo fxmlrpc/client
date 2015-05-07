c66eab1c8a5b6f3b493d01479b33e4935187d290
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-05-08 00:04:09 +0200

    Initial import

---
+++ src/MulticallBuilderInterface.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the fXmlRpc package.
+ *
+ * (c) Lars Strojny <lstrojny@php.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace fXmlRpc;
+
+/**
+ * @author Lars Strojny <lstrojny@php.net>
+ */
+interface MulticallBuilderInterface
+{
+    /**
+     * Registers a success handler applicable to all multicall responses
+     *
+     * @param callable $handler
+     *
+     * @return MulticallBuilderInterface
+     *
+     * @throws Exception\InvalidArgumentException
+     */
+    public function onSuccess(callable $handler);
+
+    /**
+     * Registers an error handler applicable to all multicall responses
+     *
+     * @param callable $handler
+     *
+     * @return MulticallBuilderInterface
+     *
+     * @throws Exception\InvalidArgumentException
+     */
+    public function onError(callable $handler);
+
+    /**
+     * Adds a call to the end of the multicall stack
+     *
+     * @param string   $methodName
+     * @param array    $params
+     * @param callable $onSuccess
+     * @param callable $onError
+     *
+     * @return MulticallBuilderInterface
+     */
+    public function addCall($methodName, array $params = [], callable $onSuccess = null, callable $onError = null);
+
+    /**
+     * Sends the multicall request to the server
+     *
+     * @return array
+     */
+    public function execute();
+}
---
+++ src/MulticallClientInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the fXmlRpc package.
+ *
+ * (c) Lars Strojny <lstrojny@php.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace fXmlRpc;
+
+/**
+ * @author Lars Strojny <lstrojny@php.net>
+ */
+interface MulticallClientInterface
+{
+    /**
+     * Start sequence of multicall
+     *
+     * @return MulticallBuilderInterface
+     */
+    public function multicall();
+}
